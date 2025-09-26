bcfdc44ec9ea69e60e01edb9774acd8c667ab973
Dm-cr7 <dennisnjeru86@gmail.com>
2025-09-26 13:29:16 +0300

    Fix: correct API base URL handling for contact form (axiosInstance)

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,11 +2,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api", 
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // set true if you plan to use cookies/auth later
+  withCredentials: true, // set to true if you plan to use cookies/auth later
 });
 
 export default axiosInstance;
